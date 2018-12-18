f1f7866c369d5b98b007ee552a1f6b63f225e334
Alesia <moyaalesia@mail.ru>
2018-12-18 14:19:50 +0000

    finish

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 module.exports = {
-    DB:'mongodb://localhost:27017/dove-app'
+    DB:'mongodb://heroku_0thmrkj2:p370rc3issj30vef2kmv8g7r07@ds147079.mlab.com:47079/heroku_0thmrkj2'
 };
 
 //localhost DB:'mongodb://localhost:27017/dove-app'
-//' DB:'mongodb://heroku_0thmrkj2:p370rc3issj30vef2kmv8g7r07@ds147079.mlab.com:47079/heroku_0thmrkj2'
+//DB:'mongodb://heroku_0thmrkj2:p370rc3issj30vef2kmv8g7r07@ds147079.mlab.com:47079/heroku_0thmrkj2'
